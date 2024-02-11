5bfdb829a67cdeb7cd0e7a6a2a8c8fcc4db79703
Назар Свистанюк <ipz223_sno@student.ztu.edu.ua>
2024-02-11 20:48:16 +0200

    Complete 1.1.1

---
+++ Tasks/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Write("Введіть перше число: ");
+            string userInput1 = Console.ReadLine();
+            double number1 = Convert.ToDouble(userInput1);
+
+            Console.Write("Введіть друге число: ");
+            string userInput2 = Console.ReadLine();
+            double number2 = Convert.ToDouble(userInput2);
+
+            double sum = number1 + number2;
+
+            Console.WriteLine("Сума двох чисел: " + sum);
+
+            Console.ReadLine();
+        }
+    }
+}
